7fc5ea04166dc20057e7bb5d85014f15457780a6
LuisMFG1992 <luismiguelfloresgr@gmail.com>
2025-03-07 10:14:39 +0100

    tailwind installed

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: ['expo', 'plugin:prettier/recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+  ignorePatterns: ['/dist/*'],
+}
--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -1,34 +1,25 @@
-import { useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withTiming,
-  withRepeat,
-  withSequence,
-} from 'react-native-reanimated';
+import { ThemedText } from '@/components/ThemedText'
+import Animated from 'react-native-reanimated'
 
-import { ThemedText } from '@/components/ThemedText';
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TouchableHighlight,
+  TouchableOpacity,
+} from 'react-native'
 
 export function HelloWave() {
-  const rotationAnimation = useSharedValue(0);
-
-  useEffect(() => {
-    rotationAnimation.value = withRepeat(
-      withSequence(withTiming(25, { duration: 150 }), withTiming(0, { duration: 150 })),
-      4 // Run the animation 4 times
-    );
-  }, []);
-
-  const animatedStyle = useAnimatedStyle(() => ({
-    transform: [{ rotate: `${rotationAnimation.value}deg` }],
-  }));
-
   return (
-    <Animated.View style={animatedStyle}>
-      <ThemedText style={styles.text}>👋</ThemedText>
+    <Animated.View>
+      <TouchableHighlight>
+        <Text>This is a fully </Text>
+      </TouchableHighlight>
+      <TouchableOpacity>
+        <Text>This is another fully customizable button</Text>
+      </TouchableOpacity>
     </Animated.View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -37,4 +28,4 @@ const styles = StyleSheet.create({
     lineHeight: 32,
     marginTop: -6,
   },
-});
+})
